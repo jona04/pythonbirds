728964dfc86ffea1d960f4000aeed4d630bf35a9
jonatasilva <jonatas.iw@gmail.com>
2020-04-21 18:29:30 -0300

    Enviar atributos de instancia Pessoa

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,2 +1,16 @@
 class Pessoa:
-    pass
+    def __init__(self, nome = None, idade=33):
+        self.idade = idade
+        self.nome = nome
+
+    def cumprimentar(self):
+        return f'Olá {id(self)}'
+
+if __name__ == '__main__':
+    p = Pessoa('Silva')
+    print(p.cumprimentar())
+    print(id(p))
+    print(p.nome)
+    p.nome = "Jonatas"
+    print(p.nome)
+    print(p.idade)
